2b00c190370e80c8987b3975d095ad56e483a03d
Alex Degtiar <alexdegtiar@gmail.com>
2023-02-27 21:23:20 -0800

    Added blink code

---
+++ blink.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""Example for Pico. Turns on the built-in LED."""
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = False

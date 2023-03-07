86d187af6053d45de6f763d0c73548d272ed7b0d
Alex Degtiar <alexdegtiar@gmail.com>
2023-03-06 20:08:40 -0800

    Update code.py for STM32F4 discovery board

--- code.py
+++ code.py
@@ -4,11 +4,15 @@ import time
 from adafruit_motor import servo
 import pwmio
 
-pwm = pwmio.PWMOut(board.GP15, duty_cycle=2**15, frequency = 50)
+led = digitalio.DigitalInOut(board.LED_BLUE)
+led.direction = digitalio.Direction.OUTPUT
+
+pwm = pwmio.PWMOut(board.PA01, duty_cycle=2**15, frequency = 50)
+"""
 pwm2 = pwmio.PWMOut(board.GP2, duty_cycle=2**15, frequency = 50)
 pwm3 = pwmio.PWMOut(board.GP3, duty_cycle=2**15, frequency = 50)
 pwm17 = pwmio.PWMOut(board.GP17, duty_cycle=2**15, frequency = 50)
-
+"""
 myservo = servo. Servo(pwm)
 #myservo1 = servo. Servo(pwm2)
 #myservo1 = servo. Servo(pwm3)
@@ -16,15 +20,15 @@ myservo = servo. Servo(pwm)
 #ask about pin 17 in discord
 
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+#led = digitalio.DigitalInOut(board.LED)
+#led.direction = digitalio.Direction.OUTPUT
 
 
 
 while True:
     myservo.angle = 90 # 0 -> 180
     led.value = True
-    time.sleep(3)
+    time.sleep(2)
     led.value = False
     myservo.angle = 180
-    time.sleep(3)
+    time.sleep(2)
